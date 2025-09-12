bd63f989b371bbe3ae728f1df48868173dcc86ce
lily <lmellis@colgate.edu>
2025-09-12 13:30:18 -0400

    error on histogram

--- Activity2.R
+++ Activity2.R
@@ -30,8 +30,10 @@ Mat.bycol[1,]
 Mat.bycol[,2]
 
 #read in weather station file from your data folder
-datW <- read.csv("Z:\\lmellis\\Data\\noaa_weather\\2011124.csv",
-                 stringsAsFactors = T)
+# TODO Change the following line
+#datW <- read.csv("Z:\\lmellis\\Data\\noaa_weather\\2011124.csv",
+#                 stringsAsFactors = T)
+datW <- read.csv("C:\\Users\\lilye\\Desktop\\GEOG 331\\noaa_weather\\2011124.csv")
 #get more information about the dataframe
 str(datW)
 
@@ -50,4 +52,38 @@ num <- c(12, 12.4, 0, 0.1, 100)
 ints <- c(1, 2, 3, 4, 5)
 
 #vector of factor data
-fac <- "clothes"(pants, shirt, dress, skirt, socks)
+clothes_vector <- c("pants", "shirt", "dress", "skirt", "socks")
+clothes_factor <- factor(clothes_vector)
+
+#find out all unique site names
+unique(datW$NAME)
+#look at the mean maximum temperature for Aberdeen
+mean(datW$TMAX[datW$NAME == "ABERDEEN, WA US"])
+#look at the mean maximum temperature for Aberdeen
+#with na.rm argument set to true to ingnore NA
+mean(datW$TMAX[datW$NAME == "ABERDEEN, WA US"], na.rm=TRUE)
+#calculate the average daily temperature
+#This temperature will be halfway between the minimum and maximum temperature
+datW$TAVE <- datW$TMIN + ((datW$TMAX-datW$TMIN)/2)
+#get the mean across all sites
+#the by function is a list of one or more variables to index over.
+#FUN indicates the function we want to use
+#if you want to specify any function specific arguments use a comma and add them after the function
+#here we want to use the na.rm arguments specific to 
+averageTemp <- aggregate(datW$TAVE, by=list(datW$NAME), FUN="mean",na.rm=TRUE)
+averageTemp
+#change the automatic output of column names to be more meaningful
+#note that MAAT is a common abbreviation for Mean Annual Air Temperature
+colnames(averageTemp) <- c("NAME","MAAT")
+averageTemp
+#convert level to number for factor data type
+#you will have to reference the level output or look at the row of data to see the character designation.
+datW$siteN <- as.numeric(datW$NAME)
+
+hist(datW$TAVE[datW$siteN == 1],
+     freq=FALSE, 
+     main = paste(levels(datW$NAME)[1]),
+     xlab = "Average daily temperature (degrees C)", 
+     ylab="Relative frequency",
+     col="grey50",
+     border="white")
