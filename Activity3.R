6738ca7eabb567fb88c06592dc1c2a044d3277c3
lily <lmellis@colgate.edu>
2025-09-28 22:11:13 -0400

    all questions answered. small changes to be made (rounding for sensor data)

--- Activity3.R
+++ Activity3.R
@@ -31,13 +31,15 @@ assert(length(a) == length(b), "error: unequal length")
 #read in the data file
 #skip the first 3 rows since there is additional column info
 #specify the the NA is designated differently
-datW <- read.csv("Z:\\lmellis\\Data\\bewkes\\bewkes_weather.csv", na.strings=c("#N/A"), skip=3, header=FALSE)
+#datW <- read.csv("Z:\\lmellis\\Data\\bewkes\\bewkes_weather.csv", na.strings=c("#N/A"), skip=3, header=FALSE)
+datW <- read.csv("C:\\Users\\lilye\\OneDrive\\Desktop\\GEOG_331\\bewkes\\bewkes_weather.csv", na.strings=c("#N/A"), skip=3, header=FALSE)
 #preview data
 print(datW[1,])
 
 # get sensor info from file
 # this data table will contain all relevant units
-sensorInfo <-   read.csv("Z:\\lmellis\\Data\\bewkes\\bewkes_weather.csv", na.strings=c("#N/A"), nrows=2)
+#sensorInfo <-   read.csv("Z:\\lmellis\\Data\\bewkes\\bewkes_weather.csv", na.strings=c("#N/A"), nrows=2)
+sensorInfo <-   read.csv("C:\\Users\\lilye\\OneDrive\\Desktop\\GEOG_331\\bewkes\\bewkes_weather.csv", na.strings=c("#N/A"), nrows=2)
 print(sensorInfo)
 
 #get column names from sensorInfo table
@@ -111,3 +113,74 @@ points(datW$DD[datW$precipitation > 0], datW$precipitation[datW$precipitation > 
 #plot lightning points only when there is lightning     
 points(datW$DD[lightscale > 0], lightscale[lightscale > 0],
        col= "tomato3", pch=19)
+
+#Question 5: test if the length of datW is the same at lightscale vector
+assert(nrow(datW) == length(lightscale), "error: unequal length")
+
+#filter out storms in wind and air temperature measurements
+# filter all values with lightning that coincides with rainfall greater than 2mm or only rainfall over 5 mm.    
+#create a new air temp column
+datW$air.tempQ2 <- ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0, NA,
+                          ifelse(datW$precipitation > 5, NA, datW$air.tempQ1))
+
+# Question 6
+datW$wind.speedQ1 <- ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0, NA,
+                            ifelse(datW$precipitation > 5, NA, datW$wind.speed))
+
+assert(all(is.na(datW$wind.speedQ1[datW$precipitation >=2 & datW$lightning.acvitivy >0 ])), "error: precipitation at or above 2 with lightning activity not filtered out")
+assert(all(is.na(datW$wind.speedQ1[datW$precipitation > 5])), "error: precipitation above 5 not filtered out")
+
+plot(datW$DD, datW$wind.speedQ1, pch=19, type = "b", xlab = "Day of Year", ylab = "Wind Speed (Filtered)")
+
+# Question 7
+#adding a month column
+datW$month<- month(dates)
+july_data <- subset(datW, month ==7)
+
+par(mfrow=c(2,2))
+plot(july_data$DD, july_data$soil.moisture, pch=19, type="b", xlab="Day of Year", ylab="Soil Moisture")
+plot(july_data$DD, july_data$soil.temp, pch=19, type="b", xlab="Day of Year", ylab="Soil Temperature")
+plot(july_data$DD, july_data$precipitation, pch=19, type="b", xlab="Day of Year", ylab="Precipitation (Filtered)")
+plot(july_data$DD, july_data$air.tempQ2, pch=19, type="b", xlab="Day of Year", ylab="Air Temp (Filtered)")
+
+#reset the plotting layout
+par(mfrow = c(1, 1))
+
+# Question 8
+#Getting all averages
+avg_air_temp <- mean(datW$air.tempQ2, na.rm = TRUE)
+avg_wind_speed <- mean(datW$wind.speedQ1, na.rm = TRUE)
+avg_soil_moisture <- mean(datW$soil.moisture, na.rm = TRUE)
+avg_soil_temp <- mean(datW$soil.temp, na.rm = TRUE)
+
+#Getting sum of precipitation
+sum_precip <- sum(datW$precipitation, na.rm = TRUE)
+
+#Getting all of the observations used
+count_air_temp <- sum(!is.na(datW$air.tempQ2))
+count_wind_speed <- sum(!is.na(datW$wind.speedQ1))
+count_soil_moisture <- sum(!is.na(datW$soil.moisture))
+count_soil_temp <- sum(!is.na(datW$soil.temp))
+count_precip <- sum(!is.na(datW$precipitation))
+
+#Time period of study
+start_date <- min(dates, na.rm = TRUE)
+end_date   <- max(dates, na.rm = TRUE)
+
+#TODO double check the sensor round (estimated right now)
+research_table <- data.frame(
+  Variable = c("Average Air Temp", "Average Wind Speed", "Average Soil Moisture", "Average Soil Temp", "Total Precipitation"),
+  Measurement = c(round(avg_air_temp, 1), round(avg_wind_speed, 1), round(avg_soil_moisture, 3), round(avg_soil_temp, 1), round(sum_precip, 1)),
+  Number_Of_Observations = c(count_air_temp, count_wind_speed, count_soil_moisture, count_soil_temp, count_precip)
+)
+print(research_table)
+cat("\nTime Period of Measurements:", format(start_date, "%Y-%m-%d %H:%M"),
+    "to", format(end_date, "%Y-%m-%d %H:%M"), "\n")
+
+# Question 9
+par(mfrow=c(2,2))
+plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab="Day of Year", ylab="Soil Moisture")
+plot(datW$DD, datW$soil.temp, pch=19, type="b", xlab="Day of Year", ylab="Soil Temperature")
+plot(datW$DD, datW$precipitation, pch=19, type="b", xlab="Day of Year", ylab="Precipitation (Filtered)")
+plot(datW$DD, datW$air.tempQ2, pch=19, type="b", xlab="Day of Year", ylab="Air Temp (Filtered)")
+
